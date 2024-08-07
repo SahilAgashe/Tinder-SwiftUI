1f2400826bac3de92cde55b7886a0788139a6217
Sahil Agashe <sahilagashe2002@gmail.com>
2024-08-07 16:18:06 +0530

    Initial Commit

---
+++ SAAProject/SAAProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAAProjectApp.swift
+//  SAAProject
+//
+//  Created by SAHIL AMRUT AGASHE on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SAAProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
